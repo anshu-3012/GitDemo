388561b22792117568110457d9ed5066e38c216c
anshu <anshupatel94@gmail.com>
2021-03-18 00:14:02 +0530

    first commit

---
+++ src/test/java/firstMaven/SeleniumTest.java
@@ -0,0 +1,18 @@
+package firstMaven;
+
+import org.testng.annotations.Test;
+public class SeleniumTest {
+	
+	@Test
+	public void BrowserM()
+	{
+		System.out.println("BrowserAutomation");
+	}
+	
+	@Test
+	public void elementsUI()
+	{
+		System.out.println("elementsUI");
+	}
+
+}
